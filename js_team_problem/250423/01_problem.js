b909d1b47e0e5277f0924bd660fb9842e0617747
sieun <sieunuies@gmail.com>
2025-04-24 17:16:17 +0900

    javascript team problem

---
+++ js_team_problem/250423/01_problem.js
@@ -0,0 +1,16 @@
+/*
+문장이 입력되면 거꾸로 출력하는 프로그램을 만들어 봅시다.
+
+입출력
+
+입력 : 거꾸로
+출력 : 로꾸거
+*/
+
+let input = '거꾸로';
+let arr = [...input];
+
+console.log(arr);
+console.log(arr.reverse())
+
+console.log(arr[0] + arr[1] + arr[2]);
