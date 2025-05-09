638d15374c2788cc66f93eb626c92fbe927fde45
sieun <sieunuies@gmail.com>
2025-05-09 11:20:13 +0900

    [250507] javascript-team_problem

--- js_team_problem/250507/03_problem.js
+++ js_team_problem/250507/03_problem.js
@@ -1,5 +1,6 @@
 /*
-버블정렬은 두 인접한 원소를 검사하여 정렬하는 방법을 말합니다. 시간 복잡도는 느리지만 코드가 단순하기 때문에 자주 사용됩니다.
+버블정렬은 두 인접한 원소를 검사하여 정렬하는 방법을 말합니다. 
+시간 복잡도는 느리지만 코드가 단순하기 때문에 자주 사용됩니다.
 
 아래 코드의 빈 칸을 채워 버블 정렬을 완성해 봅시다.
 */
@@ -8,17 +9,18 @@ function bubble(arr) {
     let result = arr.slice(); 
   
     for (let i = 0; i < result.length - 1; i++) {
-      for (/*빈칸을 채워주세요.*/) {
+      for (let j = 0; j < i+1; j++) {
           if (result[j] > result[j + 1]) {
-              //빈칸을 채워주세요.
+              let temp = result[j];
+              result[j] = result[j+1];
+              result[j+1] = temp; 
            }
-         }
-       }
-       return result;
-     }
+        }
+    }
+
+    return result;
+}
      
-     const items = prompt('입력해주세요.').split(' ').map((n) => {
-       return parseInt(n, 10);
-     });
-     
-     console.log(bubble(items));
+ const items = [4, 2, 3, 8, 5]
+ 
+ console.log(bubble(items));
